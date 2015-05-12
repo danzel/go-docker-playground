d8907d698d6507a7493f14d4ca11f135a2789eba
danzel <danzel@localhost.geek.nz>
2015-05-12 15:16:25 +1200

    Crappy http server

---
+++ server/server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gorilla/mux"
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("start.\n")
+    r := mux.NewRouter()
+    r.HandleFunc("/", HomeHandler)
+    //r.HandleFunc("/products", ProductsHandler)
+    r.HandleFunc("/products/{id}", ProductsHandler)
+
+	server := &http.Server{
+		Addr:		":35235",
+		Handler:	r,
+	}
+	server.ListenAndServe()
+    //http.Handle("/", r)
+	fmt.Printf("end.\n")
+}
+
+func HomeHandler(w http.ResponseWriter, request *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	
+	fmt.Fprintf(w, "Home request handled. <a href=\"/products/123\">Link</a>\n")
+}
+
+func ProductsHandler(w http.ResponseWriter, request *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	
+	args := mux.Vars(request)
+	
+	fmt.Fprintf(w, "Products request handled %v.\n", args["id"])
+}
+
