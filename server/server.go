db9ca632bd5b3a04bb46048a537eb4c7dce52b88
danzel <danzel@localhost.geek.nz>
2015-05-13 11:57:56 +1200

    gofmt

--- server/server.go
+++ server/server.go
@@ -1,38 +1,37 @@
 package main
 
 import (
-	"net/http"
-	"github.com/gorilla/mux"
 	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
 )
 
 func main() {
 	fmt.Printf("start.\n")
-    r := mux.NewRouter()
-    r.HandleFunc("/", HomeHandler)
-    //r.HandleFunc("/products", ProductsHandler)
-    r.HandleFunc("/products/{id}", ProductsHandler)
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	//r.HandleFunc("/products", ProductsHandler)
+	r.HandleFunc("/products/{id}", ProductsHandler)
 
 	server := &http.Server{
-		Addr:		":35235",
-		Handler:	r,
+		Addr:    ":35235",
+		Handler: r,
 	}
 	server.ListenAndServe()
-    //http.Handle("/", r)
+	//http.Handle("/", r)
 	fmt.Printf("end.\n")
 }
 
 func HomeHandler(w http.ResponseWriter, request *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	
+
 	fmt.Fprintf(w, "Home request handled. <a href=\"/products/123\">Link</a>\n")
 }
 
 func ProductsHandler(w http.ResponseWriter, request *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	
+
 	args := mux.Vars(request)
-	
+
 	fmt.Fprintf(w, "Products request handled %v.\n", args["id"])
 }
-
